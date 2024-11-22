f68c9a3eca70a52682447ecc9a5693791d14ac4f
Veeranna Hanamashetti <veeranna.mh@gmail.com>
2024-11-22 15:10:29 +0530

    aaa
    aaa

--- loan_app.py
+++ loan_app.py
@@ -3,21 +3,46 @@ import pickle
 
 app=Flask(__name__)
 
-model_file = open("classifier.pkl", "rb")
+model_file = open("classifier.pkl", "rb") # classifier.pkl
 model = pickle.load(model_file)
 
 # model.predict(inputs)
 
 # lets create end points
 
-@app.route('/',method=["GET"])
+@app.route('/', methods=["GET"])
 def home():
     return "<h1>Loan Approval Application</h1>"
 
-@app.route('/predict',method=["GET","POST"])
+
+
+@app.route('/predict', methods=["GET","POST"])
 def predict():
     if request.method=="POST":
+        loan_req = request.get_json()
+        if loan_req['Gender'] == "Male":
+            Gender = 0
+        else:
+            Gender = 1
+
+        if loan_req['Married'] == "No":
+            Married = 0
+        else:
+            Married = 1
+
+        ApplicantIncome = loan_req['ApplicantIncome']
+        LoanAmount = loan_req['LoanAmount']
+        Credit_History= loan_req['Credit_History']
+
+        result = model.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+        if result == 0:
+            pred = "Rejected"
+        else:
+            pred = "Approved"
+
         #do prediction
-        pass
+        return  {"loan_approval_status":pred}
+        # pass
     else:
         return "<h2>I will make the prediction</h2>"
+    
